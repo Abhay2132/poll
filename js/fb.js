575dcb2611b0e4f8e59828e6a557905e5ba0268d
abhay2132 <abhaybro768@gmail.com>
2022-09-11 11:38:15 +0530

    added firebase backend

---
+++ js/fb.js
@@ -0,0 +1,55 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
+import { getFirestore, collection, getDocs, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyBvJh1Dkx6EbEEMV4TJ2ZMV1TmAlAHXbbE",
+  authDomain: "sololearn-abhay.firebaseapp.com",
+  projectId: "sololearn-abhay",
+  storageBucket: "sololearn-abhay.appspot.com",
+  messagingSenderId: "1071644690524",
+  appId: "1:1071644690524:web:0a9a952c138afe13eefe99",
+  measurementId: "G-92T96BNTS5"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function postVote({ name, ans }) {
+  const questions = collection(db, 'Voting-poll');
+  const qsSnapshot = await getDocs(questions);
+  // console.log({qsSnapshot , docs : qsSnapshot.docs})
+  var qdata = qsSnapshot.docs.map(doc => doc.data());
+  var qdata = dataSanatilzer({ name, ans, data: qdata[0] });
+
+  if (qdata.error) return new Error(qdata.error)
+
+  await setDoc(doc(db, "Voting-poll", "Q1"), qdata);
+  return {
+    a1: qdata.Answers.a1.length,
+    a2: qdata.Answers.a2.length,
+    a3: qdata.Answers.a3.length,
+    a4: qdata.Answers.a4.length
+  }
+};
+
+function dataSanatilzer({ name, ans, data }) {
+  /*
+    0. Validate 'name' and 'ans'
+    1. Check for option exists or not
+    2. Remove user from submit answer
+  */
+
+  if (!data.Answers.hasOwnProperty(ans)) return { error: `Answer '${ans}' doesn't exists in '${Object.keys(data.Answers)}'` };
+
+  for (let an in data.Answers) {
+    var users = [];
+    for (let user of data.Answers[an]) {
+      if (user != name) users.push(user);
+    }
+    data.Answers[an] = users;
+  } 
+
+  data.Answers[ans].push(name)
+  return data;
+}
+
+export { postVote };
--- js/main.js
+++ js/main.js
@@ -1,45 +1,68 @@
+import { postVote } from "./fb.js";
+
+postVote({ name: "Abhay", ans: "a1" })
+	.then(console.log)
+
 const $ = (a) => document.querySelector(a);
 const $$ = (a) => document.querySelectorAll(a);
 
-function submit () {
-	let name = $(".uid").value;
+function submit() {
+	let name = $(".uid").value.trim();
 	let ans = ""
-	$$("input[name=option]").forEach(i => (i.checked && (ans = i.value)) );
-	console.table({name, ans})
-	
-	fetch(`http://phppro.rf.gz/poll/q1.php?name=${name}&ans=${ans}`)
-	.then(a => a.json())
-	.then(t => setBars(t));
+	$$("input[name=option]").forEach(i => (i.checked && (ans = i.value)));
+	console.table({ name, ans })
+
+	if (!name.match(/[a-zA-Z0-9]{5,11}/g)) return { error: `username should should be 5 - 10 character and Alpha-Numeric Only !` }
+	postVote({ name, ans })
+		.then(t => setBars(t));
 }
 
-$$("label").forEach(l => l.addEventListener("click",  function () {
+$$("label").forEach(l => l.addEventListener("click", function() {
 	$$("label").forEach(lab => lab.style.borderColor = "#333");
 	this.style.borderColor = "#00A3FF";
 }))
 
 $("form").addEventListener("submit", submit);
 
-function setBars ({a1, a2, a3, a4}) {
+function setBars({ a1, a2, a3, a4 }) {
+	let pi = (a) => parseInt(a);
+	a1 = pi(a1);
+	a2 = pi(a2);
+	a3 = pi(a3);
+	a4 = pi(a4);
+	console.log({ a1, a2, a3, a4 })
 	let total = a1 + a2 + a3 + a4;
-	let p1 = (a1 / total).toFixed(1) * 100 + '%';
-	let p2 = (a2 / total).toFixed(1) * 100 + '%';
-	let p3 = (a3 / total).toFixed(1) * 100 + '%';
-	let p4 = (a4 / total).toFixed(1) * 100 + '%';
-	
+	let p1 = a1 == 0 ? '0%' : (a1 / total).toFixed(1) * 100 + '%';
+	let p2 = a2 == 0 ? '0%' : (a2 / total).toFixed(1) * 100 + '%';
+	let p3 = a3 == 0 ? '0%' : (a3 / total).toFixed(1) * 100 + '%';
+	let p4 = a4 == 0 ? '0%' : (a4 / total).toFixed(1) * 100 + '%';
+
 	let bars = $$("label > div");
 	bars[0].style.width = p1;
 	bars[1].style.width = p2
 	bars[2].style.width = p3
 	bars[3].style.width = p4
-	
+
 	let pc = $$(".pc");
 	pc[0].innerHTML = p1
 	pc[1].innerHTML = p2
 	pc[2].innerHTML = p3
 	pc[3].innerHTML = p4
-	
+
 	$(".vote_cc").style.transform = "translateX(0)";
 	$(".total_votes").innerHTML = total;
 
-	console.table({p1, p2, p3, p4, total})
+	console.table({ p1, p2, p3, p4, total })
 }
+
+window.showError = function showError(msg) {
+	$("#msg").className = "error";
+	$("#msg").innerHTML = msg;
+}
+
+$("#msg").addEventListener("click", function(e) {
+	this.className = "note";
+	this.textContent = "Like this code so we can get more votes 😁"
+})
+
+// showError("HELLO SIR G")
